7f85d3b605c275db65799ea54bae6dfa446bf1ed
ysh2987 <ysh2987@naver.com>
2022-01-30 21:36:36 +0900

    Feat : utill

---
+++ src/components/utils/Button.js
@@ -0,0 +1,67 @@
+import React from 'react';
+import styled from 'styled-components';
+import PropTypes from 'prop-types';
+
+function Button({ text, type, event }) {
+  return (
+    <StyledButton className={type} onClick={event}>
+      {text}
+    </StyledButton>
+  );
+}
+
+Button.propTypes = {
+  text: PropTypes.string,
+  type: PropTypes.string,
+  event: PropTypes.func,
+};
+
+Button.defaultProps = {
+  text: '제출하기',
+  type: '',
+  event: () => {},
+};
+
+export default Button;
+
+const StyledButton = styled.div`
+  box-sizing: border-box;
+  width: 250px;
+  height: 40px;
+  line-height: 40px;
+  display: inline-block;
+  // background-color: ${({ theme }) => theme.lightPurple};
+  background-color: rgba(109, 102, 170, 0.7);
+  border-radius: 10px;
+  font-size: 20px;
+  color: #ffffff;
+  font-weight: 700;
+  text-align: center;
+  cursor: pointer;
+
+  &.search {
+    width: 90px;
+    height: 30px;
+    line-height: 26px;
+    background-color: #ffffff;
+    font-size: 18px;
+    color: ${({ theme }) => theme.darkPulple};
+    border: 2px solid ${({ theme }) => theme.darkPulple};
+    margin: 0;
+  }
+
+  &.stretching {
+    float: left;
+    width: 200px;
+    height: 40px;
+    line-height: 40px;
+    background-color: ${({ theme }) => theme.darkGray};
+    font-size: 18px;
+    color: #000000;
+    margin: 0;
+  }
+
+  & + & {
+    margin-left: 10%;
+  }
+`;
