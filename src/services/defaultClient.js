37b9861f85e610038410e86307416ea9cacf3d95
zeromountain <sonyeongsan@gmail.com>
2022-03-05 22:42:02 +0900

    feat: join feature connecting join API

--- src/services/defaultClient.js
+++ src/services/defaultClient.js
@@ -1,8 +1,7 @@
 import axios from 'axios';
 
 const defaultClient = axios.create({
-  baseURL: process.env.REACT_APP_SERVER_API_URL,
-  timeout: 3000,
+  baseURL: '/api',
   withCredentials: true,
 });
 
