48f0e14c47c09f98251afec7a10279aff9e8a2ee
jnhro1 <jnhro1@gmail.com>
2022-04-09 21:44:13 +0900

    #85 Fix: 포트 변경

--- src/setupProxy.js
+++ src/setupProxy.js
@@ -6,7 +6,7 @@ module.exports = function (app) {
   app.use(
     "/api",
     createProxyMiddleware({
-      target: "http://localhost:5000",
+      target: "http://localhost:6000",
       changeOrigin: true,
     })
   );
