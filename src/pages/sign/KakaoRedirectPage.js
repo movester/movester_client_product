4cf4bab64499e77858a5a70f37b5636fa5206f05
jnhro1 <jnhro1@gmail.com>
2022-05-02 20:36:34 +0900

    #144 QA: 이미 존재하는 계정 카카오 로그인시 차단

--- src/pages/sign/KakaoRedirectPage.js
+++ src/pages/sign/KakaoRedirectPage.js
@@ -1,14 +1,16 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import { useSelector, useDispatch } from 'react-redux';
 import { useNavigate } from 'react-router-dom';
 import Loading from '../../components/common/Loading';
 import { fetchKakaoLoginThunk } from '../../store/auth/authAsyncThunk';
+import LinkModal from '../../components/common/Modal/LinkModal';
 
 function KakaoRedirectPage() {
   const dispatch = useDispatch();
   const navigate = useNavigate();
 
   const isError = useSelector(state => state.auth.error);
+  const [errModalOn, setErrModalOn] = useState(false);
 
   const code = new URL(window.location.href).searchParams.get('code');
 
@@ -19,7 +21,25 @@ function KakaoRedirectPage() {
     }
   }, []);
 
-  return <Loading />;
+  useEffect(() => {
+    if (isError) {
+      setErrModalOn(() => true);
+    }
+  }, [isError]);
+
+  return (
+    <>
+      <Loading />
+      {errModalOn && (
+        <LinkModal
+          onClose={() => setErrModalOn(prev => !prev)}
+          title="카카오 로그인 실패"
+          content="이미 뭅스터 계정으로 가입된 계정입니다."
+          link="/login"
+        />
+      )}
+    </>
+  );
 }
 
 export default KakaoRedirectPage;
